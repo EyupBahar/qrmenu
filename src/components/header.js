5feac830423a8dee2a7f6aae3f0dff658d040d0e
Teacher <teacher@Teachers-MacBook-Pro.local>
2024-10-26 20:14:16 +0200

    header z-index

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Headear = () => {
   return (
-    <div style={{position:"fixed", height:"90px", width:"100%", backgroundColor:"#FEF9E2", display:"flex", boxShadow: "0px 9px 9px 1px rgba(0,0,0,0.57)", alignItems:"center", justifyContent:"center", fontFamily:"monospace", fontSize:"24px"}}>
-      <img width="120px" height="90px" src='https://marketplace.canva.com/EAFYecj_1Sc/1/0/1600w/canva-cream-and-black-simple-elegant-catering-food-logo-2LPev1tJbrg.jpg' /> ASEL PASTANESI / FAST FOOD 
+    <div style={{position:"fixed", height:"90px", width:"100%", zIndex : "9", backgroundColor:"#FEF9E2", display:"flex", boxShadow: "0px 9px 9px 1px rgba(0,0,0,0.57)", alignItems:"center", justifyContent:"center", fontFamily:"monospace", fontSize:"24px"}}>
+      <img width="120px" height="90px" src='https://marketplace.canva.com/EAFYecj_1Sc/1/0/1600w/canva-cream-and-black-simple-elegant-catering-food-logo-2LPev1tJbrg.jpg' /> ASEL PASTANESI
     </div>
   )
 }
